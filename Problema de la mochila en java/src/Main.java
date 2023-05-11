31e5a6b73bbabde7c7b58b9bdaf1380c1e62f8bf
Sergio Castellano Betancor <61086379+SCASTELLANO6044@users.noreply.github.com>
2023-05-11 20:03:31 +0100

    .

--- Problema de la mochila en java/src/Main.java
+++ Problema de la mochila en java/src/Main.java
@@ -5,16 +5,18 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String[] nombresArchivos = {"D:\\Coding\\Github projects\\Problema-de-la-mochila\\Problema de la mochila en java\\input-files\\1.txt",
-                "D:\\Coding\\Github projects\\Problema-de-la-mochila\\Problema de la mochila en java\\input-files\\2.txt",
-                "D:\\Coding\\Github projects\\Problema-de-la-mochila\\Problema de la mochila en java\\input-files\\3.txt",
-                "D:\\Coding\\Github projects\\Problema-de-la-mochila\\Problema de la mochila en java\\input-files\\4.txt"};
+//        String[] nombresArchivos = {"D:\\Coding\\Github projects\\Problema-de-la-mochila\\Problema de la mochila en java\\input-files\\1.txt",
+//                "D:\\Coding\\Github projects\\Problema-de-la-mochila\\Problema de la mochila en java\\input-files\\2.txt",
+//                "D:\\Coding\\Github projects\\Problema-de-la-mochila\\Problema de la mochila en java\\input-files\\3.txt",
+//                "D:\\Coding\\Github projects\\Problema-de-la-mochila\\Problema de la mochila en java\\input-files\\4.txt"};
+
+        String[] nombresArchivos = {"D:\\Coding\\Github projects\\Problema-de-la-mochila\\Problema de la mochila en java\\input-files\\4.txt"};
 
         List<Backpack> backpackList = FileInputReader.find(nombresArchivos);
-
+        Map<Memoization.Index, Integer> memo = new HashMap<>();
         int i = 1;
         for (Backpack backpack : backpackList){
-            System.out.println("New Memoization Result in "+i+".txt : "+ Memoization.execute(backpack.getItems(), backpack.getCapacity(), backpack.getItems().size(), new HashMap<>()));
+            System.out.println("New Memoization Result in "+i+".txt : "+ Memoization.execute(backpack.getItems(), backpack.getCapacity(), backpack.getItems().size(), memo, new Memoization.Index(0, 0)));
             System.out.println("********************************************************************************\n");
             i++;
         }
--- Problema de la mochila en java/src/Memoization.java
+++ Problema de la mochila en java/src/Memoization.java
@@ -1,7 +1,9 @@
 import java.util.*;
 
 public class Memoization {
-    public static Integer execute(List<BackpackItem> backpackItemList, Integer capacity, Integer positionIterator, Map<Memoization.Index, Integer> memo){
+
+    public static Integer execute(List<BackpackItem> backpackItemList, Integer capacity, Integer positionIterator,
+                                  Map<Memoization.Index, Integer> memo, Memoization.Index previousIndex){
         Memoization.Index index = new Index(positionIterator, capacity);
         if (memo.containsKey(index)){
             return memo.get(index);
@@ -13,27 +15,50 @@ public class Memoization {
         }
         if (backpackItemList.get(currentPositionToEvaluate).getWeight() > capacity) {
             positionIterator --;
-            result = execute(backpackItemList ,capacity, positionIterator, memo);
+            result = execute(backpackItemList ,capacity, positionIterator, memo, index);
         }else {
             BackpackItem itemEvaluated = backpackItemList.get(currentPositionToEvaluate);
             positionIterator --;
-            Integer notTakenItem = execute(backpackItemList, capacity, positionIterator, memo);
-            Integer takenItem = execute(backpackItemList, capacity - itemEvaluated.getWeight(),positionIterator, memo) + itemEvaluated.getBenefit();
-            result = maximum (notTakenItem, takenItem);
+            Integer takenItem = execute(backpackItemList, capacity - itemEvaluated.getWeight(),positionIterator, memo, index) + itemEvaluated.getBenefit();
+            Integer notTakenItem = execute(backpackItemList, capacity, positionIterator, memo, index);
+            previousIndex.setI(positionIterator);
+            if(takenItem > notTakenItem){
+                previousIndex.setW(capacity - itemEvaluated.getWeight());
+                result = takenItem;
+            }else {
+                previousIndex.setW(capacity);
+                result = notTakenItem;
+            }
         }
+        index.setPreviousIndex(previousIndex);
         memo.put(index, result);
         return result;
     }
 
-    private static Integer maximum(Integer a, Integer b){
-        if (a > b){
-            return a;
+//    private static Integer maximum(Integer a, Integer b){
+//        if (a > b){
+//            return a;
+//        }
+//        return b;
+//    }
+
+    public static class Index {
+        private int i;
+        private int w;
+        private Index previousIndex;
+        public Index(int i, int w){
+            this.i = i;
+            this.w = w;
         }
-        return b;
-    }
-
-    public record Index(int i, int w) {
-
+        public void setI(int i) {
+            this.i = i;
+        }
+        public void setW(int w) {
+            this.w = w;
+        }
+        public void setPreviousIndex(Index previousIndex) {
+            this.previousIndex = previousIndex;
+        }
         @Override
         public boolean equals(Object o) {
             if (this == o) return true;
